831773deebe736e92dfb9db22142b3ac82017b93
deluser8 <geeng.hoong@gmail.com>
2020-05-10 15:26:28 +0800

    add ip, proxy plugin

---
+++ zsh/plugin/proxy/README.md
@@ -0,0 +1,12 @@
+# proxy plugin
+
+This plugin provides command to manage proxy
+
+## Commands
+
+| Command            | Description                                   |
+| :----------------- | :-------------------------------------------- |
+| `use-proxy`        | set new proxy for current session             |
+| `use-system-proxy` | set proxy to system proxy for current session |
+| `clear-proxy`      | clear proxy settings for current session      |
+| `show-proxy`       | show proxy settings for current session       |
---
+++ zsh/plugin/proxy/proxy.plugin.zsh
@@ -0,0 +1,62 @@
+# set proxy quickly
+function use-proxy() {
+    if [ $# != 2 ] && [ $# != 1 ]
+    then
+        echo "Usage: $0 [host] <port>"
+        return -1
+    fi
+
+    local proxy
+    if [ $# = 1 ]
+    then
+        if [[ "$1" =~ ".*://.*" ]]
+        then
+            proxy="$1"
+        else
+            proxy="http://127.0.0.1:$1"
+        fi
+    else
+        proxy="http://$1:$2"
+    fi
+    echo "Use proxy $proxy"
+    export http_proxy=$proxy
+    export https_proxy=$proxy
+    export all_proxy=$proxy
+}
+
+# set proxy to system proxy quickly
+function use-system-proxy() {
+    if [[ "$(uname -s)" == "Darwin" ]]; then
+        local output=$(scutil --proxy)
+        local host port
+        if [[ "$output" =~ ".*SOCKSEnable : 1.*" ]]; then
+            port=${${output/*SOCKSPort : /}/$'\n'*/}
+            host=${${output/*SOCKSProxy : /}/$'\n'*/}
+            use-proxy "socks5://$host:$port"
+        elif [[ "$output" =~ ".*HTTPProxy : 1.*" ]]; then
+            port=${${output/*HTTPPort : /}/$'\n'*/}
+            host=${${output/*HTTPProxy : /}/$'\n'*/}
+            use-proxy "http://$host:$port"
+        elif [[ "$output" =~ ".*HTTPSProxy : 1.*" ]]; then
+            port=${${output/*HTTPSPort : /}/$'\n'*/}
+            host=${${output/*HTTPSProxy : /}/$'\n'*/}
+            use-proxy "https://$host:$port"
+        fi
+    fi
+}
+
+# clear proxy quickly
+function clear-proxy() {
+    unset http_proxy https_proxy all_proxy
+}
+
+# show proxy quickly
+function show-proxy() {
+    echo "http:  $http_proxy"
+    echo "https: $https_proxy"
+    echo "all:   $all_proxy"
+}
+
+if [[ "$USE_SYSTEM_PROXY" == "true" ]]; then
+    use-system-proxy
+fi
